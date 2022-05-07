dfae61b414074ee46d5674166ad9eec2f32d21b2
NguyenMinhThien263 <nguyenminhthien263@gmail.com>
2022-05-07 05:13:37 -0700

    Base

---
+++ IOS_BeautySelling/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOS_BeautySelling
+//
+//  Created by Thiện Nguyễn on 07/05/2022.
+//  Copyright © 2022 Thiện Nguyễn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
